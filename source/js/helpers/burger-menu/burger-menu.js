72ead2dc2c274eee62fb7ba722c29aadac574b9a
Andrew Chernigovsky <chernigovsky108@gmail.com>
2021-11-11 18:22:27 +0300

    2

--- source/js/helpers/burger-menu/burger-menu.js
+++ source/js/helpers/burger-menu/burger-menu.js
@@ -1,3 +1,8 @@
 const menu = document.getElementById('menu');
-const burger = document.querySelector('.menu__burger');
+const burger = document.querySelector('.menu__burger-btn');
+const nav = document.querySelector('.menu__nav')
 
+burger.addEventListener('click', () =>{
+  nav.classList.toggle('active');
+  console.log('2');
+});
