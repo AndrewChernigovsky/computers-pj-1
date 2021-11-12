51396260fc3e9e5fcbe7eeb4527118cb92a983ca
Andrew Chernigovsky <chernigovsky108@gmail.com>
2021-11-12 17:44:08 +0300

    1

--- source/js/helpers/burger-menu/burger-menu.js
+++ source/js/helpers/burger-menu/burger-menu.js
@@ -1,8 +1,15 @@
-const menu = document.getElementById('menu');
 const burger = document.querySelector('.menu__burger-btn');
-const nav = document.querySelector('.menu__nav')
+const nav = document.querySelector('.menu__nav');
+const jsOverlay = document.querySelector('.js-overlay-burger');
 
 burger.addEventListener('click', () =>{
-  nav.classList.toggle('active');
-  console.log('2');
+  if(nav.classList.contains('active') && burger.classList.contains('js-burger-menu')) {
+    nav.classList.toggle('active');
+    burger.classList.toggle('js-burger-menu');
+    jsOverlay.style.display = "none";
+  } else {
+    nav.classList.toggle('active');
+    burger.classList.toggle('js-burger-menu');
+    jsOverlay.style.display = "block";
+  }
 });
