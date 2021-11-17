1e12da7999635f2a7f0ed2e3c0dc56d44e9c287e
Andrew Chernigovsky <chernigovsky108@gmail.com>
2021-11-17 17:53:28 +0300

    2

---
+++ source/js/components/profile.js
@@ -0,0 +1,8 @@
+const sign = document.getElementById('sign');
+const signBurger = document.getElementById('sign-burger');
+
+const clickSing = () => {
+  
+}
+
+sign.addEventListener('click', clickSing())
