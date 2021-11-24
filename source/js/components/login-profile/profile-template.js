6e16572ed2dfba75a572d64a6f17c6dadb64fb9d
Andrew Chernigovsky <chernigovsky108@gmail.com>
2021-11-24 17:38:56 +0300

    1

--- source/js/components/login-profile/profile-template.js
+++ source/js/components/login-profile/profile-template.js
@@ -1,5 +1,7 @@
 const buttonSign = document.getElementById('sign');
+const buttonSignBurger = document.getElementById('sign-burger');
 const signEntrance = document.getElementById('sign-entrance');
+const jsOverlay = document.querySelector('.js-overlay-bg');
 
 let el = document.createElement('div');
 el.setAttribute('id','signIntro');
@@ -8,11 +10,18 @@ el.setAttribute('id','signIntro');
 function buttonSignOpen() {
   el.append(signEntrance.content.cloneNode(true))
   document.body.appendChild(el);
+  el.children[0].style.display = "block";
 };
 
 function buttonSignClose() {
-  el.remove();
+  el.remove()
+    while (this.firstChild) {this.removeChild(this.firstChild);}
+    return this;
+
+    el.children[0].style.display = "none";
 };
 
 buttonSign.addEventListener("click", buttonSignOpen)
+buttonSignBurger.addEventListener("click", buttonSignOpen)
+
 el.addEventListener("click", buttonSignClose);
